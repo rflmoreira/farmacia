7026c57b7f28d73da5a11007d760d485028edd87
rafaelmoreira-lima <rafaelmoreira.lima@hotmail.com>
2022-03-30 23:29:27 -0300

    SpringFarmacia

---
+++ model/Categoria.java
@@ -0,0 +1,45 @@
+package org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+
+
+@Entity
+@Table (name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String descricao;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	
+	
+	
+	
+}
